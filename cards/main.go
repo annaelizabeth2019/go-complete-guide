32f327c0e96ab5b728c11952d7b23335f34b3f43
annaelizabeth2019 <anna@justuno.com>
2019-09-12 16:55:18 -0500

    completes ~lesson 20

--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
+	cards := newDeck()
 
-func newCard() string {
-	return "Five of Diamonds"
+	cards.print()
 }

f29fe05a3c1dee51f25760214c8ea90559de2098
annaelizabeth2019 <anna@justuno.com>
2019-09-30 19:18:48 -0500

    completes lessons through ~47 and quiz 7

--- structs/main.go
+++ structs/main.go
@@ -2,13 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
 	age       int
+	contactInfo
 }
 
 func main() {
-	anna := person{firstName: "anna", lastName: "peterson", age: 30}
-	fmt.Println(anna)
+	jim := person{
+		firstName: "jim",
+		lastName:  "party",
+		contactInfo: contactInfo{
+			email:   "jim@spicyboy.com",
+			zipCode: 91377,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
